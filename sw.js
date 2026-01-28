1d9912d1790836d6574aff01547a27d89342078a
harshitkaushal9129-bit <harshitkaushal9129@gmail.com>
2026-01-28 08:09:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
+const CACHE_NAME = 'nielit-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://harshitkaushal9129-bit.github.io/O-level-mcqs-practice/SNT.jpg'
+];
+
+// Install Service Worker
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('nielit-v1').then((cache) => cache.addAll(['index.html']))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets...');
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetching Assets
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
